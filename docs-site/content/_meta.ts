8aeeba704d9540736843dd6abc5f00d4786343c8
Z User <z@container>
2026-03-02 10:42:49 +0000

    feat: add docs-site landing page content for GitHub Pages

---
+++ docs-site/content/_meta.ts
@@ -0,0 +1,22 @@
+export default {
+  index: {
+    title: 'Home',
+    type: 'page',
+  },
+  overview: {
+    title: 'Overview',
+    type: 'page',
+  },
+  quickstart: {
+    title: 'Quick Start',
+    type: 'page',
+  },
+  features: {
+    title: 'Features',
+    type: 'page',
+  },
+  configuration: {
+    title: 'Configuration',
+    type: 'page',
+  },
+};
